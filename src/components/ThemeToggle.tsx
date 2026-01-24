378a0b6523d1d9491c33c1304a37e3f6bca6c8aa
Rishabh Paudel <24BCS10110@cuchd.in>
2026-01-24 15:03:34 +0530

    changes in index file, + icon changed, added theme changer button & it is working right now i am working on colors selection after changing of theme. Better not to use it right now

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const ThemeToggle = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  // Run once on page load
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (isDark) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+
+    setIsDark(!isDark);
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="themeButton transition"
+    >
+      {isDark ? "🌙" : "☀️"}
+    </button>
+  );
+};
+
+export default ThemeToggle;
---
+++ src/components/ToggleSwitch.tsx
@@ -0,0 +1,18 @@
+// toggle switch for theme change, not working now therefore, 
+// I will see on to it later or never. As current feature is working fine 
+
+// import { useState } from "react";
+// import "../index.css";
+
+// export default function ToggleSwitch() {
+//   const [on, setOn] = useState(false);
+
+//   return (
+//     <div
+//       className={`toggle ${on ? "on" : ""}`}
+//       onClick={() => setOn(!on)}
+//     >
+//       <div className="knob"></div>
+//     </div>
+//   );
+// }
